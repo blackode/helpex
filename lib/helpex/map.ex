15cc60279bc46144f6ee580e965dc5b3425a8976
Blackode <mrblackode@gmail.com>
2019-11-16 12:12:06 +0530

    Map change_key function updated

--- lib/helpex/map.ex
+++ lib/helpex/map.ex
@@ -70,11 +70,11 @@ defmodule Helpex.Map do
   end
 
   @doc """
-  Returns true if `map1` is subset or `map2`, false otherwise.
+  Returns true if `map1` is subset of `map2`, false otherwise.
 
       Examples
           
-          iex> Helpex.Map.subset?(t %{a: 1}, %{a: 1, b: 2})
+          iex> Helpex.Map.subset?(%{a: 1}, %{a: 1, b: 2})
           true
 
           iex> Helpex.Map.subset?(%{a: 1, c: 3}, %{a: 1, b: 2})
@@ -95,5 +95,61 @@ defmodule Helpex.Map do
     set1 = MapSet.new(map1)
     set2 = MapSet.new(map2)
     MapSet.subset?(set1, set2)
+  end
+
+  @doc """
+  Changes the `key1` with `key2` in the given map.
+
+  If `key1` is present in a map then it is chnaged to `key2` and returns `{:ok, changed_map}`, else returns `{:error, :enokey}` 
+
+
+      Examples
+          
+          iex> Helpex.Map.change_key(%{a: 1, b: 2}, :a, :x)
+          {:ok, %{x: 1, b: 2}}
+
+          iex> Helpex.Map.change_key(%{a: 1, b: 2}, :c, :x)
+          {:error, :enokey}
+  """
+  @spec change_key(Map, String|Atom, String|Atom) :: Map
+  def change_key(map, key1, key2) do
+    if Map.has_key?(map, key1) do
+      changed_map = 
+        Map.new(map, fn 
+          {^key1, value} -> {key2, value}
+          item -> item
+        end)
+      {:ok, changed_map}
+    else
+        {:error, :enokey}
+    end
+  end
+
+  @doc """
+  Similar to `change_key/3`. 
+  
+  If `map` `key1` is changed then it gives you the changed map directly.
+
+  If `key1` is not present in `map`, a `KeyError` exception is raised.
+
+      Examples
+
+          iex> Helpex.Map.change_key!(%{a: 1, b: 2}, :a, :x)
+          %{x: 1, b: 2}
+
+          iex> Helpex.Map.change_key!(%{a: 1, b: 2}, :c, :x)
+          ** (KeyError) key :c not found in: %{a: 1, b: 2}
+
+
+  """
+  @spec change_key!(Map, String.t()|atom(), String.t()|atom()) :: map()
+  def change_key!(map, key1, key2) do
+    if Map.fetch!(map, key1) do
+      Map.new(map, fn 
+        {^key1, value} -> {key2, value}
+        item -> item
+      end)
+
+    end
   end
 end
